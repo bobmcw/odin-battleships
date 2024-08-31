c8c3599f942c16027b6f0060864155733f6acef4
bobmcw <jasiekz2014@gmail.com>
2024-08-31 19:29:40 +0200

    created translateCoordinate function

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,5 +1,39 @@
 import { ship } from "./ship";
 
 export class gameBoard {
-  constructor() {}
+  constructor() {
+    this.board = []
+    for(let i =0;i<64;i++){
+        this.board.push(i)
+    }
+    this.numOfOneLengthShips = 1
+    this.numOfTwoLengthShips = 1
+    this.numOfThreeLengthShips = 1
+    this.numOfFourLengthShips = 1
+  }
+  translateCoordinates(coordinate){
+    const dict = {
+        'a':0,
+        'b':1,
+        'c':2,
+        'd':3,
+        'e':4,
+        'f':5,
+        'g':6,
+        'h':7
+    }
+    if (coordinate[1] > 8 || coordinate[1] < 1){
+      throw new Error("row out of range")
+    }
+    if (!(coordinate[0] in dict)){
+            throw new Error("file out of range")
+    }
+    if(coordinate.length != 2){
+      throw new Error("invalid coordinate format")
+    }
+    return (coordinate[1]-1)*8 + dict[coordinate[0]]
+  }
+  placeShip(){
+
+  }
 }
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,1 +1,31 @@
 import { gameBoard } from "../src/gameboard";
+
+let board
+describe("creating a gameboard",()=>{
+    board = new gameBoard
+})
+
+test("translating simple coordinate",()=>{
+    expect(board.translateCoordinates('a1')).toBe(0)
+})
+test("coordinate from a different file",()=>{
+    expect(board.translateCoordinates('b1')).toBe(1)
+})
+test("different row",()=>{
+    expect(board.translateCoordinates('a2')).toBe(8)
+})
+test("last space",()=>{
+    expect(board.translateCoordinates('h8')).toBe(63)
+})
+test("middle file and row",()=>{
+    expect(board.translateCoordinates('c5')).toBe(34)
+})
+test("file out of range",()=>{
+    expect(() => board.translateCoordinates('m2')).toThrow()
+})
+test("row out of range",()=>{
+    expect(() => board.translateCoordinates('a9')).toThrow()
+})
+test("invalid coordinate format",()=>{
+    expect(() => board.translateCoordinates('abc2')).toThrow()
+})
