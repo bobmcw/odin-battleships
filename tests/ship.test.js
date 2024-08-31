9145f55e3c96b287c03ec7eab11800f10807928a
bobmcw <jasiekz2014@gmail.com>
2024-08-31 11:54:41 +0200

    fixed misaligned () in some tests with .toBe

--- tests/ship.test.js
+++ tests/ship.test.js
@@ -21,14 +21,14 @@ beforeEach(()=>{
 
 test('ship sinking',()=>{
     oneLengthShip.hit()
-    expect(oneLengthShip.isSunk.toBe(true))
+    expect(oneLengthShip.isSunk()).toBe(true)
 })
 test('ship not sinking after one hit if longer than one',()=>{
     twoLengthShip.hit()
-    expect(twoLengthShip.isSunk.toBe(false))
+    expect(twoLengthShip.isSunk()).toBe(false)
 })
 test('new ship is not sunk',()=>{
-    expect(oneLengthShip.isSunk.toBe(false))
+    expect(oneLengthShip.isSunk()).toBe(false)
 })
 test('ship over max length',()=>{
     expect(()=> {const illegalShip = new ship(5)}).toThrow()
