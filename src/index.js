0d01c59754a8cd578a23bdccb97533cf469960f2
bobmcw <jasiekz2014@gmail.com>
2024-09-06 13:26:15 +0200

    created DOMmethods class and added drawing the board

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 import {player} from './player.js'
 import './style.css'
+
+class DOMMethods{
+    drawBoard(player){
+        const boardContainer = document.querySelector('.board')
+        boardContainer.innerHTML = ""
+        console.log(player.getBoard())
+        player.getBoard().forEach(space => {
+            const tile = document.createElement('div')
+            tile.classList.add('tile')
+            boardContainer.appendChild(tile)
+        });
+    }
+}
+const testPlayer = new player
+const DOMTools = new DOMMethods
+DOMTools.drawBoard(testPlayer)
