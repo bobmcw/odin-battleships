efa46f933e853c551c02fb46dbc1984d91f9df89
bobmcw <jasiekz2014@gmail.com>
2024-09-08 14:29:59 +0200

    added amother empty method and renamed DOMmethods to GameController

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import {player} from './player.js'
 import './style.css'
 
-class DOMMethods{
+class GameController{
     constructor(player1,player2){
         this.player1 = player1
         this.player2 = player2
@@ -27,9 +27,12 @@ class DOMMethods{
     placeShips(){
 
     }
+    turn(){
+
+    }
 }
 const player1 = new player
 player1.placeShip(['a1','a2','a3'])
 const player2 = new player
-const DOMTools = new DOMMethods
+const DOMTools = new GameController
 DOMTools.drawBoard(player1)
