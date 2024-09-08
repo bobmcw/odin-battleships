bd0f4c10adc4c85b7748a5b968fef6492e0254dd
bobmcw <jasiekz2014@gmail.com>
2024-09-08 14:23:45 +0200

    added code for switching active player

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import {player} from './player.js'
 import './style.css'
 
 class DOMMethods{
+    constructor(player1,player2){
+        this.player1 = player1
+        this.player2 = player2
+        this.activePlayer = player1
+    }
     drawBoard(player){
         const boardContainer = document.querySelector('.board')
         boardContainer.innerHTML = ""
@@ -9,10 +14,16 @@ class DOMMethods{
         player.getBoard().forEach(space => {
             const tile = document.createElement('div')
             tile.classList.add('tile')
+            tile.innerText = space
             boardContainer.appendChild(tile)
         });
     }
+    switchPlayer(){
+        this.activePlayer === this.player1 ? this.activePlayer = this.player2 : this.activePlayer = this.player1
+    }
 }
-const testPlayer = new player
+const player1 = new player
+player1.placeShip(['a1','a2','a3'])
+const player2 = new player
 const DOMTools = new DOMMethods
-DOMTools.drawBoard(testPlayer)
+DOMTools.drawBoard(player1)
