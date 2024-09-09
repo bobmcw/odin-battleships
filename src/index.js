d4680c142e320788b9dac0ce42465de7178b9c50
bobmcw <jasiekz2014@gmail.com>
2024-09-09 21:24:52 +0200

    added an event listiner with the corrent coordinate to every tile

--- src/index.js
+++ src/index.js
@@ -10,15 +10,27 @@ class GameController{
     drawBoard(player){
         const boardContainer = document.querySelector('.board')
         boardContainer.innerHTML = ""
-        console.log(player.getBoard())
+        let coordinate = 0
         player.getBoard().forEach(space => {
+            const dict = ['a','b','c','d','e','f','g','h']
             const tile = document.createElement('div')
+            tile.value = coordinate
+            coordinate += 1
             tile.classList.add('tile')
             tile.innerText = space
+            tile.addEventListener("click",()=>{
+                console.log(tile.value)
+                let cord = ""
+                cord += dict[tile.value % 8]
+                cord += Math.trunc(tile.value/8)+1
+                console.log(cord)
+                this.activePlayer.gameboard.reciveAttack(cord)
+            })
             boardContainer.appendChild(tile)
         });
     }
     switchPlayer(){
+        //active player means which player's board is currently being displayed and therefor attacked.
         this.activePlayer === this.player1 ? this.activePlayer = this.player2 : this.activePlayer = this.player1
     }
     startGame(){
@@ -29,10 +41,11 @@ class GameController{
     }
     turn(){
 
+
     }
 }
 const player1 = new player
 player1.placeShip(['a1','a2','a3'])
 const player2 = new player
-const DOMTools = new GameController
-DOMTools.drawBoard(player1)
+const game = new GameController(player1,player2)
+game.drawBoard(player1)
