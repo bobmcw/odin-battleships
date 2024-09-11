7280cdffb1f7bd3458e01785708917a18de740f6
bobmcw <jasiekz2014@gmail.com>
2024-09-11 12:38:11 +0200

    hitting a ship now displays and X

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import "./style.css";
 
 const circle =
   '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>';
+const cross =
+  '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>';
 
 class GameController {
   constructor(player1, player2) {
@@ -41,9 +43,10 @@ class GameController {
           tile.innerHTML = circle;
           if (this.activePlayer.gameboard.reciveAttack(cord)) {
             console.log("hit");
-          }
+            tile.innerHTML = cross
           if (this.activePlayer.gameboard.getAt(cord).isSunk()) {
             console.log("sunk");
+          }
           }
           this.spacesAlreadyShot.push(cord);
         }
