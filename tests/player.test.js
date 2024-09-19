06fe6c3717cc5f1d6c98b2835aec0e31452d83db
bobmcw <jasiekz2014@gmail.com>
2024-09-19 14:23:08 +0200

    added a test for ai random shooting

--- tests/player.test.js
+++ tests/player.test.js
@@ -1,9 +1,25 @@
-import { player } from "../src/player.js";
+import { AI, player } from "../src/player.js";
 
 let testPlayer
+let testAI
 describe("creating a player",()=>{
     testPlayer = new player
+    testAI = new AI
 })
 test("new players gameboard is empty",()=>{
     expect(testPlayer.getBoard()).toStrictEqual(Array.apply(null,Array(64)))
+})
+test("AI attacks each space exactly once",()=>{
+    const spacesShot = []
+    for(let i=0;i<64;i++){
+        spacesShot.push(testAI.shootRandomSpace())
+    }
+    const validArr = []
+    for(let i=0;i<64;i++){
+        validArr.push(i)
+    }
+    spacesShot.sort((a,b)=> a-b)
+    validArr.sort((a,b)=> a-b)
+    console.log(spacesShot)
+    expect(spacesShot).toStrictEqual(validArr)
 })
